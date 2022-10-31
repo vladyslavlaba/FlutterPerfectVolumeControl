046e9d81edb0fbd3a7d2ce2dcd5b6c9981b58647
vladyslavlaba <vladyslav.laba@dsi.io>
2022-10-31 17:19:41 +0200

    - call result

--- android/src/main/java/top/huic/perfect_volume_control/perfect_volume_control/PerfectVolumeControlPlugin.java
+++ android/src/main/java/top/huic/perfect_volume_control/perfect_volume_control/PerfectVolumeControlPlugin.java
@@ -13,7 +13,6 @@ import io.flutter.plugin.common.MethodCall;
 import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
 import io.flutter.plugin.common.MethodChannel.Result;
-import io.flutter.plugin.common.PluginRegistry.Registrar;
 
 /**
  * PerfectVolumeControlPlugin
@@ -25,40 +24,43 @@ public class PerfectVolumeControlPlugin implements FlutterPlugin, MethodCallHand
     /// when the Flutter Engine is detached from the Activity
     private MethodChannel channel;
 
-    /**
-     * 音频管理器
-     */
     private AudioManager audioManager;
 
-    /**
-     * 隐藏系统音量图标
-     */
     private Boolean hideUI = false;
+
+    private VolumeReceiver volumeReceiver;
+
+    private VolumeBoundariesKeeper volumeBoundariesKeeper;
+
+    private FlutterPluginBinding binding;
+
+    private int originalVolumeLevel;
 
     @Override
     public void onAttachedToEngine(@NonNull FlutterPluginBinding flutterPluginBinding) {
-        channel = new MethodChannel(flutterPluginBinding.getBinaryMessenger(), "perfect_volume_control");
+        binding = flutterPluginBinding;
+        channel = new MethodChannel(binding.getBinaryMessenger(), "perfect_volume_control");
         channel.setMethodCallHandler(this);
-        audioManager = (AudioManager) flutterPluginBinding.getApplicationContext().getSystemService(Context.AUDIO_SERVICE);
+        audioManager = (AudioManager) binding.getApplicationContext().getSystemService(Context.AUDIO_SERVICE);
 
-        // 注册音量监听
-        VolumeReceiver volumeReceiver = new VolumeReceiver();
-        IntentFilter filter = new IntentFilter();
-        filter.addAction("android.media.VOLUME_CHANGED_ACTION");
-        flutterPluginBinding.getApplicationContext().registerReceiver(volumeReceiver, filter);
+        originalVolumeLevel = getVolume();
+        volumeBoundariesKeeper = new VolumeBoundariesKeeper();
     }
 
     @Override
     public void onMethodCall(@NonNull MethodCall call, @NonNull Result result) {
         switch (call.method) {
-            case "getVolume":
-                this.getVolume(call, result);
-                break;
-            case "setVolume":
-                this.setVolume(call, result);
-                break;
             case "hideUI":
                 this.hideUI(call, result);
+                break;
+            case "setVolumeBounds":
+                this.setVolumeBounds(call, result);
+                break;
+            case "startListeningVolume":
+                this.startListeningVolume(call, result);
+                break;
+            case "stopListeningVolume":
+                this.stopListeningVolume(call, result);
                 break;
             default:
                 result.notImplemented();
@@ -71,27 +73,13 @@ public class PerfectVolumeControlPlugin implements FlutterPlugin, MethodCallHand
         channel.setMethodCallHandler(null);
     }
 
-    /**
-     * 获得音量
-     */
-    public void getVolume(@NonNull MethodCall call, @NonNull Result result) {
-        int max = audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC);
-        int current = audioManager.getStreamVolume(AudioManager.STREAM_MUSIC);
-        result.success((double) current / (double) max);
+    public int getVolume() {
+        return audioManager.getStreamVolume(AudioManager.STREAM_MUSIC);
     }
 
-    /**
-     * 设置音量
-     */
-    public void setVolume(@NonNull MethodCall call, @NonNull Result result) {
-        Double volume = call.argument("volume");
-        if (volume == null) {
-            result.error("-1", "Volume cannot be empty", "Volume cannot be empty");
-            return;
-        }
-        int max = audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC);
-        audioManager.setStreamVolume(AudioManager.STREAM_MUSIC, (int) Math.round(max * volume), hideUI ? AudioManager.FLAG_REMOVE_SOUND_AND_VIBRATE : AudioManager.FLAG_SHOW_UI);
-        result.success(null);
+    public void setVolume(int volume) {
+        int flag = hideUI ? AudioManager.FLAG_REMOVE_SOUND_AND_VIBRATE : AudioManager.FLAG_SHOW_UI;
+        audioManager.setStreamVolume(AudioManager.STREAM_MUSIC, volume, flag);
     }
 
     /**
@@ -102,16 +90,106 @@ public class PerfectVolumeControlPlugin implements FlutterPlugin, MethodCallHand
         result.success(null);
     }
 
-    /**
-     * 音量监听
-     */
+    public void setVolumeBounds(@NonNull MethodCall call, @NonNull Result result) {
+        Double lower = call.argument("lower");
+        Double upper = call.argument("upper");
+        Boolean shouldKeep = call.argument("shouldKeep");
+
+        if (lower == null || upper == null || shouldKeep == null) {
+            result.error("-1", "Should provide boundaries for volume", null);
+            return;
+        }
+
+        volumeBoundariesKeeper = new VolumeBoundariesKeeper(lower, upper, shouldKeep);
+        volumeBoundariesKeeper.keepVolume();
+
+        result.success(null);
+    }
+
+    public void startListeningVolume(@NonNull MethodCall call, @NonNull Result result) {
+        if (volumeReceiver == null) {
+            volumeReceiver = new VolumeReceiver();
+            IntentFilter filter = new IntentFilter();
+            filter.addAction("android.media.VOLUME_CHANGED_ACTION");
+            binding.getApplicationContext().registerReceiver(volumeReceiver, filter);
+        }
+
+        result.success(null);
+    }
+
+    public void stopListeningVolume(@NonNull MethodCall call, @NonNull Result result) {
+        if (volumeReceiver != null) {
+            binding.getApplicationContext().unregisterReceiver(volumeReceiver);
+            volumeReceiver = null;
+        }
+
+        result.success(null);
+    }
+
+    public void upVolumePressed() {
+        channel.invokeMethod("volumeKeyPressed", "up");
+    }
+
+    public void downVolumePressed() {
+        channel.invokeMethod("volumeKeyPressed", "down");
+    }
+
     private class VolumeReceiver extends BroadcastReceiver {
         @Override
         public void onReceive(Context context, Intent intent) {
             if (intent.getAction().equals("android.media.VOLUME_CHANGED_ACTION")) {
-                int max = audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC);
-                int current = audioManager.getStreamVolume(AudioManager.STREAM_MUSIC);
-                channel.invokeMethod("volumeChangeListener", (double) current / (double) max);
+                int current = getVolume();
+                volumeBoundariesKeeper.keepVolume();
+
+                if (originalVolumeLevel == current) return;
+
+                if (current > originalVolumeLevel) {
+                    upVolumePressed();
+                } else {
+                    downVolumePressed();
+                }
+            }
+        }
+    }
+
+    private class VolumeBoundariesKeeper {
+        final int lower;
+        final int upper;
+        final boolean shouldKeep;
+
+        public VolumeBoundariesKeeper() {
+            int max = audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC);
+
+            this.lower = (int) Math.round(max * 1.0);
+            this.upper = (int) Math.round(max * 0.0);
+
+            this.shouldKeep = false;
+        }
+
+        public VolumeBoundariesKeeper(double lower, double upper, boolean shouldKeep) {
+            int max = audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC);
+
+            this.lower = (int) Math.round(max * lower);
+            this.upper = (int) Math.round(max * upper);
+
+            this.shouldKeep = shouldKeep;
+        }
+
+        public void keepVolume() {
+            if (!shouldKeep) return;
+
+            int newVolume = originalVolumeLevel;
+
+            if (newVolume > upper) {
+                newVolume = upper;
+            }
+            if (newVolume < lower) {
+                newVolume = lower;
+            }
+
+            int volume = getVolume();
+            if (volume != newVolume) {
+                setVolume(newVolume);
             }
         }
     }
